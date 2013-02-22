0eb99dbbc1e76a8022a344bc97a5c8f15092fb2d
sklein12 <sklein12@gmail.com>
2013-02-21 21:50:29 -0500

    adding prod environment switches in DB config

--- application/config/database.php
+++ application/config/database.php
@@ -45,25 +45,55 @@
 | the active record class
 */
 
-$active_group = 'default';
+
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'bet';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+$db['production']['hostname'] = $_SERVER["DB1_PATH"];
+$db['production']['username'] = $_SERVER["DB1_USER"];
+$db['production']['password'] = $_SERVER["DB1_PASSWORD"];
+$db['production']['database'] = $_SERVER["DB1_NAME"];
+$db['production']['port']     = $_SERVER["DB1_PORT"];
+$db['production']['dbdriver'] = "mysql";
+$db['production']['dbprefix'] = "";
+$db['production']['pconnect'] = TRUE;
+$db['production']['db_debug'] = TRUE;
+$db['production']['cache_on'] = FALSE;
+$db['production']['cachedir'] = "";
+$db['production']['char_set'] = "utf8";
+$db['production']['dbcollat'] = "utf8_general_ci";
+$db['production']['swap_pre'] = '';
+$db['production']['autoinit'] = TRUE;
+$db['production']['stricton'] = FALSE;
 
+$db['development']['hostname'] = 'localhost';
+$db['development']['username'] = 'root';
+$db['development']['password'] = 'root';
+$db['development']['database'] = 'bet';
+$db['development']['dbdriver'] = 'mysql';
+$db['development']['dbprefix'] = '';
+$db['development']['pconnect'] = TRUE;
+$db['development']['db_debug'] = TRUE;
+$db['development']['cache_on'] = FALSE;
+$db['development']['cachedir'] = '';
+$db['development']['char_set'] = 'utf8';
+$db['development']['dbcollat'] = 'utf8_general_ci';
+$db['development']['swap_pre'] = '';
+$db['development']['autoinit'] = TRUE;
+$db['development']['stricton'] = FALSE;
+
+
+
+switch (ENVIRONMENT) {
+    case 'production':
+        $active_group = 'production';
+    break;
+ 
+    // add additional cases for more environments
+ 
+    default:
+        $active_group = 'development';
+    break;
+}
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
